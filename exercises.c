e0dbaaee5d4f68cdfad9d433d813f218fb93ce2d
Your Name <you@example.com>
2024-03-16 02:45:06 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -69,7 +69,7 @@ arreglos en un tercer arreglo también ordenado.
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[])
 {
 
-  result = malloc(sizeof(int));
+  int result = malloc(sizeof(int));
   
   
   int i = 0, j = 0, k = 0;
